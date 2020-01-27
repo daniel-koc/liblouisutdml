a04e1478fbb2df98c171bbab5b6878358858c6b7
Daniel Kocielinski <daniel@openfin.co>
2020-01-27 00:57:34 -0500

    2014-08-01: added VS build files (temporary change)

---
+++ windows/makefile.bat
@@ -0,0 +1,4 @@
+set path=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin
+set INCLUDE=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\include;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+set LIB=c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib
+nmake makefile.nmake
