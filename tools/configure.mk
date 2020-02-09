5a8987836aa9ebf1892bc50825df42aed2662603
Daniel Kocielinski <daniel@openfin.co>
2020-02-09 16:22:37 -0500

    Added build files for file2brl

---
+++ tools/configure.mk
@@ -0,0 +1,7 @@
+# Change the values of the variables according to your environment.
+# The values given here should be treated as examp.les.
+# They apply only to the development machine.
+
+LIBLOUIS_PATH = ..\..\liblouis-2
+JAVA_HEADERS_PATH = "C:\Program Files\Java\jdk-13\include"
+LIBXML2_PATH = ..\..\libxml2-2
