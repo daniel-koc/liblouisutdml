7b703abb71a097fd61d0d48a20e72b3d53e6521b
Daniel Kocielinski <daniel@openfin.co>
2020-02-09 16:22:37 -0500

    2014-08-01: Added build files for file2brl

---
+++ tools/configure.mk
@@ -0,0 +1,7 @@
+# Change the values of the variables according to your environment.
+# The values given here should be treated as examp.les.
+# They apply only to the development machine.
+
+LIBLOUIS_PATH = c:\users\User\PlatMat\liblouis-2.5.4
+JAVA_HEADERS_PATH = "c:\Program Files (x86)\Java\jdk1.8.0_11\include"
+LIBXML2_PATH = c:\users\User\PlatMat\libxml2-2.7.2
