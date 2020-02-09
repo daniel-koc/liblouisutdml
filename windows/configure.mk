f4f84ad46ecedd7f73077e6ab28d1e745b7a6da2
Daniel Kocielinski <daniel@openfin.co>
2020-02-09 16:25:35 -0500

    2014-08-01: Modified build files (renamed library name from liblouis-2 into liblouis, added copying liblouis.dll)

--- windows/configure.mk
+++ windows/configure.mk
@@ -2,7 +2,6 @@
 # The values given here should be treated as examp.les.
 # They apply only to the development machine.
 
-LIBLOUIS_PATH = c:\liblouis
-JAVA_HEADERS_PATH = "c:\Program Files\Java\jdk1.6.0_23\include"
-LIBXML2_PATH = c:\libxml2-2.7.2
-
+LIBLOUIS_PATH = c:\users\User\PlatMat\louis\liblouis-2
+JAVA_HEADERS_PATH = "c:\Program Files (x86)\Java\jdk1.8.0_11\include"
+LIBXML2_PATH = c:\users\User\PlatMat\louis\libxml2-2
